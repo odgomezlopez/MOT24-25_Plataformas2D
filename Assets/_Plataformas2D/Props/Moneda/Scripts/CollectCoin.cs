ba903609cf9a31e49d46399492d2139a020cb071
Oscar <odgomezlopez@gmail.com>
2024-11-14 18:56:43 +0100

    Mejora audio
    Simplificación del sonido de la moneda. Pendiente. Mover PlaySoundAtPoint a Utils. Lo veremos más adelante.

--- Assets/_Plataformas2D/Props/Moneda/Scripts/CollectCoin.cs
+++ Assets/_Plataformas2D/Props/Moneda/Scripts/CollectCoin.cs
@@ -1,28 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class CollectCoin : MonoBehaviour
 {
-    //Versión 1. Utilizando el instanciador de AudioSource, pero falla un poco en el efecto Doppler
     [SerializeField] AudioClip audioClip;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            if (audioClip) AudioSource.PlayClipAtPoint(audioClip,Camera.main.transform.position,1);
+            if (audioClip)
+            {
+                //AudioSource.PlayClipAtPoint(audioClip, Camera.main.transform.position, 1);//Opción 1. Utilizando AudioSource
+                PlaySoundAtPoint(audioClip,Camera.main.transform.position);
+            }
+            
             Destroy(gameObject);
         }
     }
 
-    //Versión 2. Instanciando Prefabs con AudioSource con PlayOnAwake y el Script DestroyWhenEnd
-    /*[SerializeField] GameObject coinSFX;
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void PlaySoundAtPoint(AudioClip clip, Vector3 position, float volume = 1, float pitch = 1)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            if(coinSFX) Instantiate(coinSFX,null);
-            Destroy(gameObject);
-        }
-    }*/
+        if (clip == null) return;
+
+        var tempGO = new GameObject("TempAudio") { transform = { position = position } };
+        var audioSource = tempGO.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+
+        audioSource.Play();
+        Destroy(tempGO, clip.length / Mathf.Abs(pitch));
+    }
 }
